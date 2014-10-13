1a3c24bb6bacb09f3d5d07a75a6b501c1c4e4647
Marc <marc.bluemner@gmail.com>
2014-10-13 16:45:35 +0200

    added avatar params

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -69,6 +69,6 @@ class MembersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def member_params
-      params.require(:member).permit(:first_name, :last_name, :description)
+      params.require(:member).permit(:first_name, :last_name, :description, :avatar)
     end
 end
